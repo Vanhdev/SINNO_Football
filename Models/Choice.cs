62b0c148fe3cdd0fb38bff85343002b6a34275fc
anhtv <vietanhtran2069@gmail.com>
2024-03-17 02:02:14 +0700

    Update

---
+++ Models/Choice.cs
@@ -0,0 +1,18 @@
+﻿using BsonData;
+
+namespace System
+{
+    partial class Document
+    {
+        public DateTime? Date { get => GetDateTime(nameof(Date)); set => Push(nameof(Date), value); }
+        public List<KeyValuePair<string, bool>> MemberVote { get => GetArray<List<KeyValuePair<string, bool>>>(nameof(MemberVote)); set => Push(nameof(MemberVote), value); }
+    }
+}
+
+namespace System
+{
+    partial class DB
+    {
+        static public Collection Choices => Main.GetCollection(nameof(Choices));
+    }
+}
--- Models/_system/Actors.cs
+++ Models/_system/Actors.cs
@@ -1,9 +1,24 @@
-﻿namespace Actors
+﻿using System.Runtime.CompilerServices;
+using System.Security.Principal;
+
+namespace Actors
 {
     public partial class Admin : Account
     {
     }
     public partial class Member : Account
     {
+        public static int SetTotalPoint(Document doc)
+        {
+            switch (doc.Position)
+            {
+                case "ST": return (int)Math.Round((doc.Pac * 0.2) + (doc.Sho * 0.35) + (doc.Pas * 0.1) + (doc.Dri * 0.1) + (doc.Def * 0.1) + (doc.Phy * 0.15), MidpointRounding.AwayFromZero);
+                case "CM": return (int)Math.Round((doc.Pac * 0.1) + (doc.Sho * 0.15) + (doc.Pas * 0.35) + (doc.Dri * 0.15) + (doc.Def * 0.15) + (doc.Phy * 0.1), MidpointRounding.AwayFromZero);
+                case "LB": case "RB": return (int)Math.Round((doc.Pac * 0.25) + (doc.Sho * 0.05) + (doc.Pas * 0.2) + (doc.Dri * 0.1) + (doc.Def * 0.25) + (doc.Phy * 0.15), MidpointRounding.AwayFromZero);
+                case "CB": return (int)Math.Round((doc.Pac * 0.05) + (doc.Sho * 0.05) + (doc.Pas * 0.15) + (doc.Dri * 0.1) + (doc.Def * 0.4) + (doc.Phy * 0.25), MidpointRounding.AwayFromZero);
+                case "GK": return (int)Math.Round((doc.Pas * 0.3) + (doc.GKH * 0.7), MidpointRounding.AwayFromZero);
+                default: return 70;
+            }
+        }
     }
 }
