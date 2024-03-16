83ebe673f7f0f9c9f8ad8254281561ee1b23c2ee
anhtv <vietanhtran2069@gmail.com>
2024-03-17 02:11:31 +0700

    Update vote

--- Models/Choice.cs
+++ Models/Choice.cs
@@ -6,6 +6,8 @@ namespace System
     {
         public DateTime? Date { get => GetDateTime(nameof(Date)); set => Push(nameof(Date), value); }
         public List<KeyValuePair<string, bool>> MemberVote { get => GetArray<List<KeyValuePair<string, bool>>>(nameof(MemberVote)); set => Push(nameof(MemberVote), value); }
+        public bool ChoiceRespone { get => GetValue<bool>(nameof(ChoiceRespone)); set => Push(nameof(ChoiceRespone), value); }
+        public string? Label { get => GetString(nameof(Label)); set => Push(nameof(Label), value); }
     }
 }
 
